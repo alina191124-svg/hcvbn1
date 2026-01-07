c75e4a71e064b0a24ecd003a6bf192cc7d3fd17c
Алина <alina191124@gmail>
2026-01-07 18:13:35 +0300

    8

--- 8pd.py
+++ 8pd.py
@@ -1,4 +1,4 @@
 import pandas as pd
 df=pd.read_csv('titanic.csv')
 print(len(df[(df['Sex']=='female')&(df['Pclass']==1)&(df['Survived']==1)]))
-print(len(df[(df['Age']<18)&(df['Parch'] == 0)]))
+print(len(df[(df['Age']<18)&(df['Parch']==0)]))
