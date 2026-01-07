e72af297d1cb83a154a824277c60d847a585b499
Алина <alina191124@gmail>
2026-01-07 13:58:44 +0300

    commit

---
+++ 1pd.py
@@ -0,0 +1,8 @@
+import pandas as pd
+df=pd.read_csv('titanic.csv')
+print("Кол-во сторк,столбцов,тип данных")
+df.info()
+print('Кол-во пассажиров: ', len(df))
+print('Кол-во пропущенных значений в каждом столбце')
+print(df.isnull().sum())
+print(df['Age'].isnull().sum())
